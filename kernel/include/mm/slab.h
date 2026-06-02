fa625685f2b6eeea0c60b3922174f1cca0fb0526
STierProgrammer <sabasajaia40@gmail.com>
2026-06-02 19:03:30 +0400

    (Impl): Implemented cache_create for the slab allocator

--- kernel/include/mm/slab.h
+++ kernel/include/mm/slab.h
@@ -4,3 +4,37 @@
 #include <stdint.h>
 #include <vendor/list.h>
 
+/* TODO: Add colouring */
+/* TODO: Add flags */
+typedef struct
+{
+    list_t list;
+    list_t slabs_free;
+    list_t slabs_partial;
+    list_t slabs_full;
+    
+    size_t obj_size;
+    size_t num_objs_per_slab;
+
+    size_t total_num_objs;
+    size_t total_num_slabs;
+    
+
+    /* It's usually 1 */
+    size_t num_pages_per_slab;
+    const char *name;
+} cache_t;
+
+typedef struct
+{
+    list_t list;
+    
+    size_t num_objs_inuse;
+    void *smem;
+    // TODO: prev pointer is not needed
+    list_t freelist;
+} slab_t;
+
+cache_t *cache_create(const char *name, size_t obj_size);
+
+
--- kernel/src/mm/slab.c
+++ kernel/src/mm/slab.c
@@ -1,1 +1,30 @@
+#include <kernel.h>
+#include <mm/vheap.h>
+#include <vendor/list.h>
+#include <mm/slab.h>
+
+// TODO: Maybe tweak with alignment
+// TODO: This will corrupt memory if obj_size < sizeof(uintptr_t)
+cache_t *cache_create(const char *name, size_t obj_size)
+{
+    // TODO: Don't use vmalloc
+    cache_t *cache = vmalloc(sizeof(cache_t));
+    
+    list_init(&cache->list);
+    list_init(&cache->slabs_free);
+    list_init(&cache->slabs_partial);
+    list_init(&cache->slabs_full);
+    
+    cache->obj_size = obj_size;
+    // TODO: Change this to a better method
+    cache->num_pages_per_slab = 1;
+    cache->num_objs_per_slab = cache->num_pages_per_slab * PAGE_SIZE / cache->obj_size;
+    cache->total_num_objs = 0;
+    cache->total_num_slabs = 0;
+    cache->name = name;
+
+    return cache;
+}
+
+
 
